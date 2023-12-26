9e47ac5d4087f560cbe44421ff9ce3bbea709360
lolakkalol <alexander.stenlund@telia.com>
2023-12-26 16:33:08 +0100

    First attempt at 32 bit floting point arithmetic

---
+++ programs/estimate-pi/main.asm
@@ -0,0 +1,69 @@
+; Author: Alexander Stenlund
+; Description: This is a project template for easy creation 
+; of 6502 applications
+
+;  +-----------+
+;  |  IMPORTS  |
+;  +===========+
+;.import LSR32A
+;.import LDA32I
+.import FADD32A
+.import LF32A
+.import LF32B
+
+;  +-----------+
+;  |  EXPORTS  |
+;  +===========+
+.export A32, B32, A16, FLOAT_A, FLOAT_B
+
+;  +----------------+
+;  |  DATA SEGMENT  |
+;  +================+
+;
+; Description: Add initilized read/write data here. This 
+; data will be copied into RAM at startup, use labes as 
+; usual.
+.segment "DATA"
+
+;  +----------------+
+;  |   ZP SEGMENT   |
+;  +================+
+;
+; Description: The zero page of memory in SRAM, will be 
+; initilized.
+.zeropage
+A32:  .DWORD $41200000
+B32:  .DWORD $C1300000
+A16:  .WORD 0
+; Unpacked 32 bit float
+; | 3 Bytes mantissa | 1 Byte exponent | 1 Byte sign (+/-) |
+FLOAT_A: .BYTE 0, 0, 0, 0, 0
+FLOAT_B: .BYTE 0, 0, 0, 0, 0
+
+;  +------------------+
+;  |  RODATA SEGMENT  |
+;  +==================+
+;
+; Description: Add static data here. This data will be
+; located inside the ROM.
+.segment "RODATA"
+
+;  +----------------+
+;  |  CODE SEGMENT  |
+;  +================+
+;
+; Description: This section contains all of the "main" code,
+; this will be the first executable code (After the copy of
+; DATA into RAM in LOAD section).
+.segment "CODE"
+
+; MAIN SEGMENT :D
+
+LDA #A32
+JSR LF32A
+LDA #B32
+JSR LF32B
+JSR FADD32A
+
+; Stop the program from progressing further
+END_: JMP END_
---
+++ programs/estimate-pi/start.asm
@@ -0,0 +1,82 @@
+; Author: Alexander Stenlund
+; Description: This file is used for all code needed to 
+; initilize/start the program in ROM. It has two main 
+; responsibilities: 1) load the data in the DATA section 
+; into RAM and 2) Set the reset vector in ROM.
+
+.import __CODE_RUN__
+
+.import __LOAD_RUN__
+
+.import __DATA_LOAD__
+.import __DATA_RUN__
+.import __DATA_SIZE__
+
+.import __ZEROPAGE_LOAD__
+.import __ZEROPAGE_RUN__
+.import __ZEROPAGE_SIZE__
+
+;  +----------------+
+;  |  LOAD SEGMENT  |
+;  +================+
+;
+; Description: Loads the DATA segment into RAM. Currently 
+; only handels one page of memory, i.e. $FF.
+.segment "LOAD"
+
+; :------------------------------:
+; :----: LOAD DATA INTO RAM :----:
+; :------------------------------:
+
+LDX #.LOBYTE(__DATA_SIZE__)
+LDY #.HIBYTE(__DATA_SIZE__)
+
+; Stop execution if DATA section is over 255 bytes
+inf: BNE inf
+
+; Skip copying if low byte is zero
+CPX #$0
+BEQ DATA_LOAD_END
+
+; Copy DATA in rom to DATA in RAM
+data_cp_loop:
+    DEX
+    LDA __DATA_LOAD__, X  ; Loads the DATA at DATA_ROM + X
+    STA __DATA_RUN__, X   ; Stores the DATA at DATA_RAM + X
+    CPX #$0
+    BNE data_cp_loop
+
+DATA_LOAD_END:
+
+; :----------------------------:
+; :----: LOAD ZP INTO RAM :----:
+; :----------------------------:
+
+LDX #.LOBYTE(__ZEROPAGE_SIZE__)
+LDY #.HIBYTE(__ZEROPAGE_SIZE__)
+
+; Stop execution if DATA section is over 255 bytes
+BNE inf
+
+; Skip copying if low byte is zero
+CPX #$0
+BEQ LOAD_END
+
+; Copy DATA in rom to DATA in RAM
+zp_cp_loop:
+    DEX
+    LDA __ZEROPAGE_LOAD__, X  ; Loads the DATA at DATA_ROM + X
+    STA __ZEROPAGE_RUN__, X   ; Stores the DATA at DATA_RAM + X
+    CPX #$0
+    BNE zp_cp_loop
+
+; Jump to main code
+LOAD_END: jmp __CODE_RUN__
+
+
+;  +----------------+
+;  |  RESET VECTOR  |
+;  +================+
+
+.segment "PC_START"
+.WORD __LOAD_RUN__
